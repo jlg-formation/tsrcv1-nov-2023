a4d5790b04b275d1133db7654fe47fd93b213575
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-11-13 14:15:00 +0100

    dev: added svg circle elements

--- front/main.js
+++ front/main.js
@@ -1,3 +1,16 @@
 console.log("coucou");
 
+const svgns = "http://www.w3.org/2000/svg";
+
 // boucle qui genere du SVG et qui l'insert au bon endroit
+const gSample = document.querySelector("g.samples");
+if (gSample === null) {
+  throw new Error("oups");
+}
+for (let i = 0; i < 10; i++) {
+  const circle = document.createElementNS(svgns, "circle");
+  circle.setAttributeNS(null, "cx", "34");
+  circle.setAttributeNS(null, "cy", "52");
+  circle.setAttributeNS(null, "r", "1");
+  gSample.appendChild(circle);
+}
